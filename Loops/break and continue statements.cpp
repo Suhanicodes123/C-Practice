d146af238ffdb595a8d917d377356955b7a0fa68
SuhaniCodes <Suhani190507@gmail.com>
2026-07-26 20:13:34 +0530

    Create break and continue statements.cpp

---
+++ Loops/break and continue statements.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    for(int i=0; i<40; i++)
+    {
+        if(i==2){
+            continue;
+        }
+        cout<<i<<endl;
+    }
+    
+}
+
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    for(int i=0; i<40; i++)
+    {
+        if(i==2){
+            break;
+        }
+        cout<<i<<endl;
+    }
+    
+}
