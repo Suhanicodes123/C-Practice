7033c0a11a9dbb7a2f9fb5ff3d134a8437a2a4d5
SuhaniCodes <Suhani190507@gmail.com>
2026-07-26 19:41:37 +0530

    Create for_loops.cpp

---
+++ Loops/for_loops.cpp
@@ -0,0 +1,16 @@
+// syntax of for loop
+// for(initilization; condition; updation)
+//  {
+//      loop body(C++ code);
+//  }    
+
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    for (int i = 1; i<=40; i++)
+    {
+        cout<<i<<endl;
+    }
+}
