ba45176a590b950eb027f33751b289b7fb266b59
SuhaniCodes <Suhani190507@gmail.com>
2026-07-26 19:44:13 +0530

    Create do while loops.cpp

---
+++ Loops/do while loops.cpp
@@ -0,0 +1,17 @@
+// syntax of do while loop
+// do{
+//     C++ statements;
+// }while(condition);
+
+
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    int i=1;
+    do{
+        cout<<i<<endl;
+        i++;
+    }while(i<=20);
+}
