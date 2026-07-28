e9a546bd66a7d17914e88f1e986501d139c818f1
Suhanicodes123 <Suhani190507@gmail.com>
2026-07-28 18:59:11 +0530

    Update operator precedence and associavity.cpp

--- Basics/operator precedence and associavity.cpp
+++ Basics/operator precedence and associavity.cpp
@@ -7,4 +7,6 @@ int main(){
     int c = ((((a*5)+b)-45)+87);
     cout<<c;
     return 0;
+// Operator precedence helps us to solve an expression
+// When two or more operators have the same precedence, the operator associativity helps us to decide that we should solve the expression from "left-to-right" or from "right-to-left".
 }
