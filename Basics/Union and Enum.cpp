33ff14cd71855cbb4a365321565517646087bdb1
SuhaniCodes <Suhani190507@gmail.com>
2026-07-30 18:17:34 +0530

    Create Union and Enum.cpp

---
+++ Basics/Union and Enum.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+
+union money
+{
+    int rice;
+    char car;
+    float pounds;
+    
+};
+
+int main(){
+    union money m1;
+    m1.rice = 34;
+    m1.car = 'c';
+    cout<<m1.rice; 
+    // returns garbage value as we can only use one at a time as memory is shared among all three
+}    
+
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    enum Meal{breakfast, lunch, dinner};
+    // enum use as data type assign nos
+    Meal m1 = lunch;
+    cout<<(m1==1);
+}
