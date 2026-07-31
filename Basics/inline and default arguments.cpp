48e043f9422972b7e7e608ad5b6b682ceba34cdd
SuhaniCodes <Suhani190507@gmail.com>
2026-07-31 18:34:11 +0530

    Create inline and default arguments.cpp

---
+++ Basics/inline and default arguments.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+inline int product(int a, int b){
+    // static int c=0;
+    // this will execute only one time
+    // c = c+1;
+    // when the function is run next time, the value of c will be retained
+    return a*b;
+}
+float moneyReceived(int currentMoney, float factor=1.04){
+    return currentMoney * factor;
+    // compulsory argument should be on left and default argument should be on right side
+}
+int main() {
+    int a, b;
+    cout<<"Enter value of a and b "<<endl;
+    cin>>a>>b;
+    cout<<"the product of a and b is "<<product(a, b)<<endl;
+    int money = 100;
+    cout<<"if you have "<<money<<" Rs in your bank account, you will receive "<<moneyReceived(money)<< " Rs after one year";
+    
+}
