4f51aab183a53e754c9ddf0ee95b4ca43fec31fe
Suhanicodes123 <Suhani190507@gmail.com>
2026-08-01 21:23:13 +0530

    Update Constants and Manipulators.cpp

--- Basics/Constants and Manipulators.cpp
+++ Basics/Constants and Manipulators.cpp
@@ -21,6 +21,7 @@ int main(){
     cout<<"The value of a without setw is: "<<a<<endl;
     cout<<"The value of b without setw is: "<<b<<endl;
     cout<<"The value of c without setw is: "<<c<<endl;
+
 // setw(4) will set there width to 4
     cout<<"The value of a is: "<<setw(4)<<a<<endl;
     cout<<"The value of b is: "<<setw(4)<<b<<endl;
