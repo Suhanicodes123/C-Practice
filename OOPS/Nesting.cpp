90ddd427cf7c7a5bad3895d3d91fccdbd35ff738
SuhaniCodes <Suhani190507@gmail.com>
2026-08-04 19:57:26 +0530

    Create Nesting.cpp

---
+++ OOPS/Nesting.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+class binary
+{
+    string s;
+    
+public:
+    void read(void);
+    void check_bin(void);
+};
+
+void binary :: read(void){
+    cout<<"enter a binary number"<<endl;
+    cin>>s;
+}
+
+void binary :: check_bin(void){
+    for (int i = 0; i < s.length(); i++)
+    {
+        if(s.at(i)!='0' && s.at(i)!='1'){
+            cout<<"incorrect binary format"<<endl;
+            exit(0);
+        }
+    }
+}    
+    
+int main(){
+    binary b;
+    b.read();
+    
+    b.check_bin();
+}
