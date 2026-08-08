4d7c58c32556352387a295581b466940eb82999d
SuhaniCodes <Suhani190507@gmail.com>
2026-08-08 18:50:11 +0530

    Create Member friend function.cpp

---
+++ OOPS/Member friend function.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+using namespace std;
+// forward declaration
+
+class Complex;
+
+class Calculator{
+    public:
+        int add(int a, int b){
+            return (a + b);
+        }
+        
+        int sumRealComplex(Complex, Complex );
+};
+
+class Complex{
+    int a, b;
+    friend int Calculator :: sumRealComplex(Complex, Complex);
+    public:
+        void setNumber(int n1, int n2){
+            a = n1;
+            b= n2;
+        }
+       
+       void printNumber(){
+           cout<<"your number is "<<a<<" + "<<b<<"i"<<endl;
+       } 
+};
+
+int Calculator :: sumRealComplex(Complex o1, Complex o2){
+    return (o1.a + o2.a);
+}
+
+int main(){
+    Complex o1, o2;
+    o1.setNumber(1, 4);
+    o2.setNumber(5, 7);
+    Calculator calc;
+    int result = calc.sumRealComplex(o1, o2);
+    cout<<" the sum of real part of o1 and o2 is "<<result<<endl;
+    return 0;
+}
